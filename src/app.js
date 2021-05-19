ac8d705f6e62582d88bef8e621b18f6118cb1f15
fsdexter <fsdexter@gmail.com>
2021-05-19 19:05:53 +0000

    app added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn_NE").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = newExcuse();
+  });
+};
+let newExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var rdmvaleu1 = Math.floor(Math.random() * who.length);
+  var rdmvaleu2 = Math.floor(Math.random() * what.length);
+  var rdmvaleu3 = Math.floor(Math.random() * when.length);
+
+  return who[rdmvaleu1] + " " + what[rdmvaleu2] + " " + when[rdmvaleu3];
 };
